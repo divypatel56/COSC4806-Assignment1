86e628c23019a138d5fced1174064c9821cffade
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-05-08 22:36:56 +0000

    Created login page with basic form - not functional

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Divy</title>
+    <title>index</title>
   </head>
   
   <body>
     <h1>Assignment-1</h1>
+    
+    <p><a href ="./login.php">Click here to login.</a></p>
     
   </body>
 </html>
